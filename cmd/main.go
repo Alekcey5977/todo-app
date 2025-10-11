69ece8f19b226cd5894faaeb27ee03db3118aec9
Alekcey7795 <alekcey7795@mail.ru>
2025-10-11 18:48:54 +0700

    feat: add service and repository interfaces and structure

--- cmd/main.go
+++ cmd/main.go
@@ -5,11 +5,15 @@ import (
 
 	"github.com/Alekcey5977/todo-app"
 	"github.com/Alekcey5977/todo-app/pkg/handler"
+	"github.com/Alekcey5977/todo-app/pkg/repository"
+	"github.com/Alekcey5977/todo-app/pkg/service"
 )
 
 func main() {
 	// Entry point for the todo application
-	handler := new(handler.Handler)
+	repos := repository.NewRepository()
+	service := service.NewService(repos)
+	handler := handler.NewHandler(service)
 
 	srv := new(todo.Server)
 	if err := srv.Run(":8000", handler.InitRoutes()); err != nil {
