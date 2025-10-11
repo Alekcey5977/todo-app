ec6c6ecba09d062999c8a8105875b97c67dd17ff
Alekcey7795 <alekcey7795@mail.ru>
2025-10-11 20:14:03 +0700

    feat: implement configuration initialization and database schema setup

--- cmd/main.go
+++ cmd/main.go
@@ -7,16 +7,28 @@ import (
 	"github.com/Alekcey5977/todo-app/pkg/handler"
 	"github.com/Alekcey5977/todo-app/pkg/repository"
 	"github.com/Alekcey5977/todo-app/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
 	// Entry point for the todo application
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing config: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	service := service.NewService(repos)
 	handler := handler.NewHandler(service)
 
 	srv := new(todo.Server)
-	if err := srv.Run(":8000", handler.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handler.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }
